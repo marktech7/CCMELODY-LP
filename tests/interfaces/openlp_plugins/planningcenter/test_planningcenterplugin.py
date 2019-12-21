797bb4e56b7be7f9499aac2fc9052c81353c4250
Tim <tim.bentley@gmail.com>
2019-12-21 20:02:34 +0000

    Remove import

--- tests/interfaces/openlp_plugins/planningcenter/test_planningcenterplugin.py
+++ tests/interfaces/openlp_plugins/planningcenter/test_planningcenterplugin.py
@@ -22,7 +22,7 @@
 Package to test the openlp.plugins.planningcenter.planningcenterplugin package.
 """
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 from PyQt5 import QtWidgets
 
