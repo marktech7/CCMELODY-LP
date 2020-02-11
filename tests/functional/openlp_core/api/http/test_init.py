aaf762115366986b630f0890f84675d8d69b75fa
Tim <tim.bentley@gmail.com>
2020-02-11 17:02:59 +0000

    API Test Cleanup

--- tests/functional/openlp_core/api/http/test_init.py
+++ tests/functional/openlp_core/api/http/test_init.py
@@ -21,146 +21,133 @@
 """
 Functional tests to test the Http init.
 """
-from unittest import TestCase
 from unittest.mock import MagicMock
 
 from openlp.core.api.http import authenticate, check_auth, requires_auth
-from openlp.core.common.registry import Registry
-from openlp.core.common.settings import Settings
-from tests.helpers.testmixin import TestMixin
 
 
-class TestInit(TestCase, TestMixin):
+password = 'c3VwZXJmbHk6bGFtYXM='
+
+
+def test_auth(settings):
     """
-    A test suite to test the functions on the init
+    Test the check_auth method with a match
+    :return:
     """
+    # GIVEN: a known user
+    settings.setValue('api/user id', "superfly")
+    settings.setValue('api/password', "lamas")
 
-    def setUp(self):
-        """
-        Create the UI
-        """
-        Registry().create()
-        Registry().register('service_list', MagicMock())
-        self.build_settings()
-        self.password = 'c3VwZXJmbHk6bGFtYXM='
-        Registry().register('settings', Settings())
+    # WHEN : I check the authorisation
+    is_valid = check_auth(['aaaaa', password])
 
-    def tearDown(self):
-        self.destroy_settings()
+    # THEN:
+    assert is_valid is True
 
-    def test_auth(self):
-        """
-        Test the check_auth method with a match
-        :return:
-        """
-        # GIVEN: a known user
-        Settings().setValue('api/user id', "superfly")
-        Settings().setValue('api/password', "lamas")
 
-        # WHEN : I check the authorisation
-        is_valid = check_auth(['aaaaa', self.password])
+def test_auth_falure(settings):
+    """
+    Test the check_auth method with a match
+    :return:
+    """
+    # GIVEN: a known user
+    settings.setValue('api/user id', 'superfly')
+    settings.setValue('api/password', 'lamas')
 
-        # THEN:
-        assert is_valid is True
+    # WHEN : I check the authorisation
+    is_valid = check_auth(['aaaaa', 'monkey123'])
 
-    def test_auth_falure(self):
-        """
-        Test the check_auth method with a match
-        :return:
-        """
-        # GIVEN: a known user
-        Settings().setValue('api/user id', 'superfly')
-        Settings().setValue('api/password', 'lamas')
+    # THEN:
+    assert is_valid is False
 
-        # WHEN : I check the authorisation
-        is_valid = check_auth(['aaaaa', 'monkey123'])
 
-        # THEN:
-        assert is_valid is False
+def test_requires_auth_disabled(settings):
+    """
+    Test the requires_auth wrapper with disabled security
+    :return:
+    """
+    # GIVEN: A disabled security
+    settings.setValue('api/authentication enabled', False)
 
-    def test_requires_auth_disabled(self):
-        """
-        Test the requires_auth wrapper with disabled security
-        :return:
-        """
-        # GIVEN: A disabled security
-        Settings().setValue('api/authentication enabled', False)
+    # WHEN: I call the function
+    wrapped_function = requires_auth(func)
+    value = wrapped_function()
 
-        # WHEN: I call the function
-        wrapped_function = requires_auth(func)
-        value = wrapped_function()
+    # THEN: the result will be as expected
+    assert value == 'called'
 
-        # THEN: the result will be as expected
-        assert value == 'called'
 
-    def test_requires_auth_enabled(self):
-        """
-        Test the requires_auth wrapper with enabled security
-        :return:
-        """
-        # GIVEN: A disabled security
-        Settings().setValue('api/authentication enabled', True)
+def test_requires_auth_enabled(settings):
+    """
+    Test the requires_auth wrapper with enabled security
+    :return:
+    """
+    # GIVEN: A disabled security
+    settings.setValue('api/authentication enabled', True)
 
-        # WHEN: I call the function
-        wrapped_function = requires_auth(func)
-        req = MagicMock()
-        value = wrapped_function(req)
+    # WHEN: I call the function
+    wrapped_function = requires_auth(func)
+    req = MagicMock()
+    value = wrapped_function(req)
 
-        # THEN: the result will be as expected
-        assert str(value) == str(authenticate())
+    # THEN: the result will be as expected
+    assert str(value) == str(authenticate())
 
-    def test_requires_auth_enabled_auth_error(self):
-        """
-        Test the requires_auth wrapper with enabled security and authorization taken place and and error
-        :return:
-        """
-        # GIVEN: A enabled security
-        Settings().setValue('api/authentication enabled', True)
 
-        # WHEN: I call the function with the wrong password
-        wrapped_function = requires_auth(func)
-        req = MagicMock()
-        req.authorization = ['Basic', 'cccccccc']
-        value = wrapped_function(req)
+def test_requires_auth_enabled_auth_error(settings):
+    """
+    Test the requires_auth wrapper with enabled security and authorization taken place and and error
+    :return:
+    """
+    # GIVEN: A enabled security
+    settings.setValue('api/authentication enabled', True)
 
-        # THEN: the result will be as expected - try again
-        assert str(value) == str(authenticate())
+    # WHEN: I call the function with the wrong password
+    wrapped_function = requires_auth(func)
+    req = MagicMock()
+    req.authorization = ['Basic', 'cccccccc']
+    value = wrapped_function(req)
 
-    def test_requires_auth_enabled_auth(self):
-        """
-        Test the requires_auth wrapper with enabled security and authorization taken place and and error
-        :return:
-        """
-        # GIVEN: An enabled security and a known user
-        Settings().setValue('api/authentication enabled', True)
-        Settings().setValue('api/user id', 'superfly')
-        Settings().setValue('api/password', 'lamas')
+    # THEN: the result will be as expected - try again
+    assert str(value) == str(authenticate())
 
-        # WHEN: I call the function with the wrong password
-        wrapped_function = requires_auth(func)
-        req = MagicMock()
-        req.authorization = ['Basic', self.password]
-        value = wrapped_function(req)
 
-        # THEN: the result will be as expected - try again
-        assert str(value) == 'called'
+def test_requires_auth_enabled_auth(settings):
+    """
+    Test the requires_auth wrapper with enabled security and authorization taken place and and error
+    :return:
+    """
+    # GIVEN: An enabled security and a known user
+    settings.setValue('api/authentication enabled', True)
+    settings.setValue('api/user id', 'superfly')
+    settings.setValue('api/password', 'lamas')
 
-    def test_requires_auth_missing_credentials(self):
-        """
-        Test the requires_auth wrapper with enabled security and authorization taken place and and error
-        :return:
-        """
-        # GIVEN: An enabled security and a known user
-        Settings().setValue('api/authentication enabled', True)
-        Settings().setValue('api/user id', 'superfly')
-        Settings().setValue('api/password', 'lamas')
+    # WHEN: I call the function with the wrong password
+    wrapped_function = requires_auth(func)
+    req = MagicMock()
+    req.authorization = ['Basic', password]
+    value = wrapped_function(req)
 
-        # WHEN: I call the function with no password
-        wrapped_function = requires_auth(func)
-        value = wrapped_function(0)
+    # THEN: the result will be as expected - try again
+    assert str(value) == 'called'
 
-        # THEN: the result will be as expected (unauthorized)
-        assert str(value) == str(authenticate())
+
+def test_requires_auth_missing_credentials(settings):
+    """
+    Test the requires_auth wrapper with enabled security and authorization taken place and and error
+    :return:
+    """
+    # GIVEN: An enabled security and a known user
+    settings.setValue('api/authentication enabled', True)
+    settings.setValue('api/user id', 'superfly')
+    settings.setValue('api/password', 'lamas')
+
+    # WHEN: I call the function with no password
+    wrapped_function = requires_auth(func)
+    value = wrapped_function(0)
+
+    # THEN: the result will be as expected (unauthorized)
+    assert str(value) == str(authenticate())
 
 
 def func(field=None):
