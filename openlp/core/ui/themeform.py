62a6ebaeed38f477ce980d22e33da9eaaed99b61
robbiejackson <robbie.jackson@btinternet.com>
2021-08-17 20:42:15 +0100

    help included in settings form

--- openlp/core/ui/themeform.py
+++ openlp/core/ui/themeform.py
@@ -53,7 +53,7 @@ class ThemeForm(QtWidgets.QWizard, Ui_ThemeWizard, RegistryProperties):
         :param parent: The QWidget-derived parent of the wizard.
         """
         super(ThemeForm, self).__init__(parent, QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint |
-                                           QtCore.Qt.WindowCloseButtonHint)
+                                        QtCore.Qt.WindowCloseButtonHint)
         self._setup()
 
     def _setup(self):
