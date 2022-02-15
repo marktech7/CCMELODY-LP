7f2a860a0648d1e6ffbd593de828dd6195c41754
Ken Roberts <alisonken1@gmail.com>
2022-02-14 16:53:16 -0800

    Update tests for projector manager 2022-02-13
    tests/openlp_core/projector/
        - manager/test_bootstrap.py
            - expanded tests

--- tests/openlp_core/projectors/manager/test_boostrap.py
+++ tests/openlp_core/projectors/manager/test_boostrap.py
@@ -22,73 +22,135 @@
 Test ProjectorManager.bootstrap_* methods
 """
 import logging
+import openlp.core.projectors.manager
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import DEFAULT, MagicMock, patch
 
-from openlp.core.projectors.db import ProjectorDB
-from openlp.core.projectors.editform import ProjectorEditForm
+test_module = openlp.core.projectors.manager.__name__
 
 
-def test_bootstrap_initialise(projector_manager_memdb):
+def test_bootstrap_initialise(projector_manager, caplog):
     """
-    Test initialize calls correct startup functions
+    Test ProjectorManager initializes with existing ProjectorDB instance
     """
-    # WHEN: we call bootstrap_initialise
-    projector_manager_memdb.bootstrap_initialise()
-    # THEN: ProjectorDB is setup
-    assert type(projector_manager_memdb.projectordb) is ProjectorDB, \
-        'Initialization should have created a ProjectorDB() instance'
+    # GIVEN: Test setup
+    caplog.set_level(logging.DEBUG)
+    logs = [(test_module, logging.DEBUG, 'Using existing ProjectorDB() instance')]
+
+    with patch.multiple(projector_manager,
+                        setup_ui=DEFAULT,
+                        get_settings=DEFAULT) as mock_manager, \
+         patch('openlp.core.projectors.manager.ProjectorDB') as mock_db:
+
+        # WHEN: we call bootstrap_initialise
+        caplog.clear()
+        projector_manager.bootstrap_initialise()
+
+        # THEN: Appropriate entries and actions
+        assert caplog.record_tuples == logs, 'Invalid log entries'
+        mock_manager['setup_ui'].assert_called_once()
+        mock_manager['get_settings'].assert_called_once()
+        mock_db.assert_not_called()
 
 
 def test_bootstrap_initialise_nodb(projector_manager_nodb, caplog):
     """
-    Test log entry creating new projector DB
+    Test ProjectorManager initializes with a new ProjectorDB instance
     """
     # GIVEN: Test setup
     caplog.set_level(logging.DEBUG)
-    log_entries = 'Creating new ProjectorDB() instance'
+    logs = [(test_module, logging.DEBUG, 'Creating new ProjectorDB() instance')]
 
-    # WHEN: ProjectorManager created with no DB set
-    caplog.clear()
-    projector_manager_nodb.bootstrap_initialise()
-    # THEN: Log should indicate new DB being created
-    assert caplog.messages[3] == log_entries, "ProjectorManager should have indicated a new DB being created"
+    with patch.multiple(projector_manager_nodb,
+                        setup_ui=DEFAULT,
+                        get_settings=DEFAULT) as mock_manager, \
+         patch('openlp.core.projectors.manager.ProjectorDB') as mock_db:
+
+        # WHEN: we call bootstrap_initialise
+        caplog.clear()
+        projector_manager_nodb.bootstrap_initialise()
+
+        # THEN: Appropriate entries and actions
+        assert caplog.record_tuples == logs, 'Invalid log entries'
+        mock_manager['setup_ui'].assert_called_once()
+        mock_manager['get_settings'].assert_called_once()
+        mock_db.assert_called_once()
 
 
-def test_bootstrap_post_set_up(projector_manager_memdb):
+def test_bootstrap_post_set_up_autostart_false(projector_manager_memdb, settings, caplog):
     """
     Test post-initialize calls proper setups
     """
-    # GIVEN: setup mocks
-    projector_manager_memdb._load_projectors = MagicMock()
-
-    # WHEN: Call to initialize is run
-    projector_manager_memdb.bootstrap_initialise()
-    projector_manager_memdb.bootstrap_post_set_up()
-
-    # THEN: verify calls to retrieve saved projectors and edit page initialized
-    assert 1 == projector_manager_memdb._load_projectors.call_count, \
-        'Initialization should have called load_projectors()'
-    assert type(projector_manager_memdb.projector_form) == ProjectorEditForm, \
-        'Initialization should have created a Projector Edit Form'
-    assert projector_manager_memdb.projectordb is projector_manager_memdb.projector_form.projectordb, \
-        'ProjectorEditForm should be using same ProjectorDB() instance as ProjectorManager'
-
-
-def test_bootstrap_post_set_up_autostart_projector(projector_manager_memdb, caplog):
-    """
-    Test post-initialize calling log and QTimer on autostart
-    """
-    # GIVEN: Setup mocks
+    # GIVEN: Test setup
     caplog.set_level(logging.DEBUG)
-    log_entries = 'Delaying 1.5 seconds before loading all projectors'
+    logs = [(test_module, logging.DEBUG, 'Loading all projectors')]
 
-    with patch('openlp.core.projectors.manager.QtCore.QTimer.singleShot') as mock_timer:
-        # WHEN: Initializations called
-        projector_manager_memdb.bootstrap_initialise()
-        projector_manager_memdb.autostart = True
+    mock_timer = MagicMock()
+    mock_newProjector = MagicMock()
+    mock_editProjector = MagicMock()
+    mock_edit = MagicMock()
+    mock_edit.newProjector = mock_newProjector
+    mock_edit.editProjector = mock_editProjector
+
+    settings.setValue('projector/connect on start', False)
+    projector_manager_memdb.bootstrap_initialise()
+
+    with patch('openlp.core.projectors.manager.ProjectorEditForm') as mocked_edit, \
+         patch('openlp.core.projectors.manager.QtCore') as mock_core, \
+         patch.multiple(projector_manager_memdb,
+                        _load_projectors=DEFAULT,
+                        projector_list_widget=DEFAULT) as mock_manager:
+        mocked_edit.return_value = mock_edit
+        mock_core.QTimer = mock_timer
+
+        # WHEN: Call to initialize is run
+        caplog.clear()
         projector_manager_memdb.bootstrap_post_set_up()
 
-        # THEN: verify log entries and timer calls
-        mock_timer.assert_called_once_with(1500, projector_manager_memdb._load_projectors)
-        assert caplog.messages[-1] == log_entries, "Invalid log entries"
+        # THEN: verify calls and logs
+        mock_timer.singleShot.assert_not_called()
+        mock_newProjector.connect.assert_called_once()
+        mock_editProjector.connect.assert_called_once()
+        mock_manager['_load_projectors'].assert_called_once(),
+        mock_manager['projector_list_widget'].itemSelectionChanged.connect.assert_called_once()
+        assert caplog.record_tuples == logs, 'Invalid log entries'
+
+
+def test_bootstrap_post_set_up_autostart_true(projector_manager_memdb, settings, caplog):
+    """
+    Test post-initialize calls proper setups
+    """
+    # GIVEN: Test setup
+    caplog.set_level(logging.DEBUG)
+    logs = [(test_module, logging.DEBUG, 'Delaying 1.5 seconds before loading all projectors')]
+
+    mock_qtimer = MagicMock()
+    mock_newProjector = MagicMock()
+    mock_editProjector = MagicMock()
+    mock_edit = MagicMock()
+    mock_edit.newProjector = mock_newProjector
+    mock_edit.editProjector = mock_editProjector
+
+    settings.setValue('projector/connect on start', True)
+    projector_manager_memdb.bootstrap_initialise()
+
+    with patch('openlp.core.projectors.manager.ProjectorEditForm') as mocked_edit, \
+         patch('openlp.core.projectors.manager.QtCore') as mock_core, \
+         patch.multiple(projector_manager_memdb,
+                        setup_ui=DEFAULT,
+                        _load_projectors=DEFAULT,
+                        projector_list_widget=DEFAULT) as mock_manager:
+        mocked_edit.return_value = mock_edit
+        mock_core.QTimer = mock_qtimer
+
+        # WHEN: Call to initialize is run
+        caplog.clear()
+        projector_manager_memdb.bootstrap_post_set_up()
+
+        # THEN: verify calls and logs
+        mock_qtimer.assert_called_once()
+        mock_newProjector.connect.assert_called_once()
+        mock_editProjector.connect.assert_called_once()
+        mock_manager['_load_projectors'].assert_not_called(),
+        mock_manager['projector_list_widget'].itemSelectionChanged.connect.assert_called_once()
+        assert caplog.record_tuples == logs, 'Invalid log entries'
