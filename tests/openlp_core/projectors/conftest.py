65386793911fb6c60cd97556cc1d0fcea7ed0b8d
Ken Roberts <alisonken1@gmail.com>
2022-03-04 15:56:51 -0800

    Fix ProjectorDB.get_projector oops - add conftest.projectordb fixture

--- tests/openlp_core/projectors/conftest.py
+++ tests/openlp_core/projectors/conftest.py
@@ -21,6 +21,7 @@
 """
 Fixtures for projector tests
 """
+import os
 import pytest
 
 from unittest.mock import patch
@@ -84,6 +85,20 @@ def fake_pjlink():
     faker = FakePJLink()
     yield faker
     del(faker)
+
+
+@pytest.fixture()
+def projectordb(temp_folder, settings):
+    """
+    Set up anything necessary for all tests
+    """
+    tmpdb_url = 'sqlite:///{db}'.format(db=os.path.join(temp_folder, TEST_DB))
+    with patch('openlp.core.projectors.db.init_url') as mocked_init_url:
+        mocked_init_url.return_value = tmpdb_url
+        proj = ProjectorDB()
+    yield proj
+    proj.session.close()
+    del proj
 
 
 @pytest.fixture()
