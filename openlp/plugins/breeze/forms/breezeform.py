f94ce122852923cc6c86898c15703446f00a08b1
Benjamin Plain <bplain@blackhawkelectronics.com>
2023-10-19 11:52:03 -0400

    Working song import

--- openlp/plugins/breeze/forms/breezeform.py
+++ openlp/plugins/breeze/forms/breezeform.py
@@ -34,6 +34,7 @@ from PyQt5 import QtCore, QtWidgets
 from openlp.core.common.i18n import translate
 from openlp.core.common.registry import Registry
 from openlp.plugins.bibles.lib import parse_reference
+from openlp.plugins.bibles.lib.mediaitem import BibleMediaItem
 from openlp.plugins.breeze.forms.selectserviceplandialog import Ui_SelectPlanDialog
 from openlp.plugins.breeze.lib.customimport import BreezeCustomImport
 from openlp.plugins.breeze.lib.breeze_api import BreezeAPI
@@ -82,14 +83,12 @@ class BreezeForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
             return
 
         # Fetch the Plans within a week
-        today = date.today()
-        week_ago = today - timedelta(days=7)
-        week_later = today + timedelta(days=7)
-        event_list = self.api.get_event_instances(week_ago, week_later)
+        event_list = self._fetch_events()
+        # Get Today's date and see if it is listed... if it is, then select it in the combobox
         self.plan_selection_combo_box.clear()
         self.plan_selection_combo_box.addItem('Select Service Plan Date')
         self.plan_selection_combo_box.setCurrentIndex(0)
-        # Get Today's date and see if it is listed... if it is, then select it in the combobox
+        today = date.today()
 
         for event in event_list:
             # start_datetime=str: 2023-09-26 00:00:00
@@ -103,7 +102,7 @@ class BreezeForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
             if plan_date >= today:
                 self.plan_selection_combo_box.setCurrentIndex(self.plan_selection_combo_box.count() - 1)
                 self.import_as_new_button.setEnabled(True)
-                self.update_existing_button.setEnabled(True)
+                # self.update_existing_button.setEnabled(True)
         # Set the 2 lists of themes
         theme_manager = Registry().get('theme_manager')
         for theme in theme_manager.get_theme_names():
@@ -192,7 +191,7 @@ class BreezeForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
                         # TODO: Waiting for Breeze Service Plan Songs
                         # Interface with Song Plugin to find song in local library or CCLI
                         # Add slide from there! We are not importing lyrics at all.
-                        song_db: SongsPlugin = Registry().get("songs_plugin")
+                        song_db: SongsPlugin = Registry().get("songs").plugin
                         # Could get the songs (a SongMediaItem) and use search(string), which does an everything search.
                         # Check DB first if song exists so we don't overwrite anything
                         # If title starts with a number and a space, look in default songbook for the number.
@@ -209,6 +208,7 @@ class BreezeForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
                         if search:
                             # Let's add the first one for now
                             openlp_id = search[0].id
+                            print("Found song id %d", openlp_id)
                             # TODO: If not there and we have a CCLI ID and the songselect is enabled, then init import
                         else:
                             # Else, create a song slide with just the title
@@ -217,24 +217,17 @@ class BreezeForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
                             song_import.title = segment['title']
                             song_import.add_verse('')
                             openlp_id = song_import.finish(temporary_flag=True)
+                            print("Created temp song, id %d", openlp_id)
                             if segment['updated_at']:
                                 song = song_db.manager.get_object(Song, openlp_id)
                                 song.last_modified = datetime.strptime(segment['updated_at']
                                                                        .rstrip("Z"), '%Y-%m-%dT%H:%M:%S')
                                 song_db.manager.save_object(song)
-                    if openlp_id >= 0:
-                        segment_id_to_openlp_id[song_id] = openlp_id
                         media_type = 'songs'
                         media_type_suffix = 'song'
-                        media_type_plugin = Registry().get(media_type)
-
-                        # turn on remote song feature to add to service
-                        media_type_plugin.remote_triggered = True
-                        setattr(media_type_plugin, "remote_{0}".format(media_type_suffix), openlp_id)
-                        media_type_plugin.add_to_service(remote=openlp_id)
-                elif segment['type'] is 'scripture':
+                elif segment['type'] == 'scripture':
                     # get the bible manager
-                    bible_media = Registry().get('bibles')
+                    bible_media: BibleMediaItem = Registry().get('bibles')
                     bibles = bible_media.plugin.manager.get_bibles()
                     # get the current bible selected from the bibles plugin screen
                     bible = bible_media.version_combo_box.currentText()
@@ -243,7 +236,6 @@ class BreezeForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
                         # one or more bibles available, use one of those
                         bible = next(iter(bibles))
                     language_selection = bible_media.plugin.manager.get_language_selection(bible)
-                    # replace long dashes with normal dashes -- why do these get inserted in PCO?
                     tmp_item_title = re.sub('–', '-', item_title)
                     ref_list = parse_reference(tmp_item_title, bibles[bible], language_selection)
                     if ref_list:
@@ -251,51 +243,41 @@ class BreezeForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
                         bible_media.list_view.clear()
                         bible_media.display_results()
                         bible_media.add_to_service()
-                elif segment['type'] is 'general':
+                    else:
+                        print("Could not resolve scripture for %s", item_title)
+                elif segment['type'] == 'general':
                     theme_name = self.slide_theme_selection_combo_box.currentText()
-                    BreezeCustomImport().add_slide(segment, theme_name)
+                    openlp_id = BreezeCustomImport().add_slide(segment, theme_name)
+                    media_type = 'custom'
+                    media_type_suffix = 'custom'
+                    # TODO: Handle Attachments (images/media) and URL's
                 # Don't care about sections, notes or any new types.
+
+                if openlp_id >= 0:
+                    segment_id_to_openlp_id[song_id] = openlp_id
+
+                if media_type:
+                    media_type_plugin = Registry().get(media_type)
+                    # turn on remote song feature to add to service
+                    media_type_plugin.remote_triggered = True
+                    setattr(media_type_plugin, "remote_{0}".format(media_type_suffix), openlp_id)
+                    media_type_plugin.add_to_service(remote=openlp_id)
 
                 # End of importing plan segment
                 service_manager.main_window.increment_progress_bar()
-            if update:
-                for old_service_item in old_service_items:
-                    # see if this service_item contained within the current set of service items
-                    # see if we have this same value in the new service
-                    for service_index, service_item in enumerate(service_manager.service_items):
-                        # we can compare songs to songs and custom to custom but not between them
-                        if old_service_item['service_item'].name == 'songs' and \
-                                service_item['service_item'].name == 'songs':
-                            if old_service_item['service_item'].audit == service_item['service_item'].audit:
-                                # get the timestamp from the xml of both the old and new and compare...
-                                # modifiedDate="2018-06-30T18:44:35Z"
-                                old_match = re.search('modifiedDate="(.+?)Z*"',
-                                                      old_service_item['service_item'].xml_version)
-                                old_datetime = datetime.strptime(old_match.group(1), '%Y-%m-%dT%H:%M:%S')
-                                new_match = re.search('modifiedDate="(.+?)Z*"',
-                                                      service_item['service_item'].xml_version)
-                                new_datetime = datetime.strptime(new_match.group(1), '%Y-%m-%dT%H:%M:%S')
-                                # if old timestamp is more recent than new, then copy old to new
-                                if old_datetime > new_datetime:
-                                    service_manager.service_items[service_index] = old_service_item
-                                break
-                        elif old_service_item['service_item'].name == 'custom' and \
-                                service_item['service_item'].name == 'custom':
-                            """ we don't get actual slide content from the V2 PC API, so all we create by default is a
-                            single slide with matching title and content.  If the content
-                            is different between the old serviceitem (previously imported
-                            from PC and the new content that we are importing now, then
-                            the assumption is that we updated this content and we want to
-                            keep the old content after this update.  If we actually updated
-                            something on the PC site in this slide, it would have a
-                            different title because that is all we can get the v2API """
-                            if old_service_item['service_item'].title == service_item['service_item'].title:
-                                if old_service_item['service_item'].slides != service_item['service_item'].slides:
-                                    service_manager.service_items[service_index] = old_service_item
-                                break
             service_manager.main_window.finished_progress_bar()
             # select the first item
             segment = service_manager.service_manager_list.topLevelItem(0)
             service_manager.service_manager_list.setCurrentItem(segment)
             service_manager.repaint_service_list(-1, -1)
             service_manager.application.set_normal_cursor()
+
+    def _fetch_events(self):
+        log.debug("Fetching Breeze Events")
+
+        today = date.today()
+        week_ago = today - timedelta(days=7)
+        week_later = today + timedelta(days=7)
+        event_list = self.api.get_event_instances(week_ago, week_later)
+
+        return event_list
